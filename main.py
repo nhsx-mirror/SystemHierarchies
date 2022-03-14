c4d6cff5a8ca65ee287814ee312d0cdfa0c35833
faaiz-25 <mfaaiz.shanawas@gmail.com>
2022-03-14 17:27:23 +0000

    started making funnel chart

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-import plotly
+import plotly.express as px
+
+data = dict(number = [100, 40, 20, 19], stage = ["Trusts", "CCG", "ICS", "Regions"])
+
+fig = px.funnel(data, x= 'number', y = 'stage')
+fig.show()
