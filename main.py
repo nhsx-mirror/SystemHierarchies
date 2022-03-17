c38ca0a10e436fa11bcab1063cc34809a21e4b86
faaiz-25 <mfaaiz.shanawas@gmail.com>
2022-03-17 13:48:07 +0000

    generated plotly figures and saved as html files
    created plotly figures for several mappins from regions through to gp practices. This includes funnel charts, sunburst charts and treemaps and saved the outputs as html files

--- main.py
+++ main.py
@@ -1,6 +1,61 @@
 import plotly.express as px
+from plotly import graph_objects as go
+import pandas as pd
+#import chart_studio.tools as tls
 
-data = dict(number = [100, 40, 20, 19], stage = ["Trusts", "CCG", "ICS", "Regions"])
+df_gp = pd.read_csv('/Users/muhammad-faaiz.shanawas/Documents/GitHub/SystemHierarchies/data/gp-reg-pat-prac-map.csv')
+list_of_ccgs = df_gp['CCG_CODE'].unique()
+num_of_ccgs = len(list_of_ccgs)
 
-fig = px.funnel(data, x= 'number', y = 'stage')
-fig.show()
+list_of_pcns = df_gp['PCN_NAME'].unique()
+num_of_pcns = len(list_of_pcns)
+
+
+list_of_stps = df_gp['STP_NAME'].unique()
+num_of_stps = len(list_of_stps)
+
+#load in data and retrieve the number of trusts
+df_trusts = pd.read_csv('/Users/muhammad-faaiz.shanawas/Documents/GitHub/SystemHierarchies/data/etr.csv', header = None)
+list_of_trusts = df_trusts[1].unique()
+num_of_trusts = len(list_of_trusts)
+
+df_gp_cut = df_gp[['PRACTICE_NAME', 'PCN_NAME', 'CCG_NAME', 'STP_NAME', 'COMM_REGION_NAME']]
+
+
+#create dict and display funnel chart
+
+data_funnel = dict(number = [7, num_of_stps, num_of_ccgs, num_of_trusts, num_of_pcns, 6528], stage = ["Regions", "ICS", "CCG", "Trusts", "PCN", "GP Practices"])
+
+fig = px.funnel(data_funnel, x= 'number', y = 'stage')
+#fig.show()
+
+
+#create a sunburst chart mapping regions-stps-ccgs-pcn-practices
+print(df_gp_cut)
+fig2 = px.sunburst(df_gp_cut, path = ['COMM_REGION_NAME', 'STP_NAME', 'CCG_NAME', 'PCN_NAME', 'PRACTICE_NAME'], values = None)
+#fig2.show()
+
+
+#create a smaller sunburst chart mapping regions-stps-ccgs
+fig3 = px.sunburst(df_gp_cut, path = ['COMM_REGION_NAME', 'STP_NAME', 'CCG_NAME'], values = None)
+#fig3.show()
+
+
+#create a treemap mapping regions-stps-ccgs-pcns-practices
+fig4 = px.treemap(df_gp_cut, path = ['COMM_REGION_NAME', 'STP_NAME', 'CCG_NAME', 'PCN_NAME', 'PRACTICE_NAME'], values = None)
+#fig4.show()
+
+
+#create a smaller treemap mapping regions-stps-ccgs
+fig5 = px.treemap(df_gp_cut, path = ['COMM_REGION_NAME', 'STP_NAME', 'CCG_NAME'], values = None)
+#fig5.show()
+
+#saving all plotly figures
+'''
+fig.write_html("/Users/muhammad-faaiz.shanawas/Documents/GitHub/SystemHierarchies/map outputs/funnel_chart.html")
+fig2.write_html("/Users/muhammad-faaiz.shanawas/Documents/GitHub/SystemHierarchies/map outputs/sunburst_large.html")
+fig3.write_html("/Users/muhammad-faaiz.shanawas/Documents/GitHub/SystemHierarchies/map outputs/sunburst_small.html")
+fig4.write_html("/Users/muhammad-faaiz.shanawas/Documents/GitHub/SystemHierarchies/map outputs/treemap_large.html")
+fig5.write_html("/Users/muhammad-faaiz.shanawas/Documents/GitHub/SystemHierarchies/map outputs/treemap_small.html")
+'''
+
